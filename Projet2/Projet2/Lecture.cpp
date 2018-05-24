71d7cb47dd1dba6a2fb2cc6d638007ad672a3d32
AlanPa <alan.paugois@gmail.com>
2018-05-24 10:53:43 +0200

    Classe Lecture codée. Il reste à faire les différents test de cette classe

--- Projet2/Projet2/Lecture.cpp
+++ Projet2/Projet2/Lecture.cpp
@@ -1,7 +1,7 @@
 using namespace std;
 #include <string>
 #include "Lecture.h"
-//Commentaire
+
 Lecture::Lecture(string fichierMeta, string fichierMal, string fichierPat) {
 #ifdef MAP
 	cout << "Appel au constructeur de <Lecture>" << endl;
@@ -9,12 +9,90 @@ Lecture::Lecture(string fichierMeta, string fichierMal, string fichierPat) {
 	nomFichierMeta = fichierMeta;
 	nomFichierMal = fichierMal;
 	nomFichierPat = fichierPat;
-<<<<<<< HEAD
-	string s;
-=======
-	//some bla bla
->>>>>>> dabcdcf547f9dfb24f4cae8cbb6626b72dab0241
+
+	ifstream fichier(nomFichierMeta, ios::in);  // on ouvre en lecture
+	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
+	getline(fichier, contenu);
+	while(getline(fichier, contenu);)  
+	{
+			metadonnees.push_back(contenu);
+	}
+	fichier.close();
+                
 }
+
+
+void Lecture::LireMaladies(vector<Maladie> &tabMaladie)
+{
+	ifstream fichier(nomFichierMal, ios::in);  // on ouvre en lecture
+	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
+	getline(fichier, contenu);
+	while(getline(fichier, contenu);)  
+	{
+			tabMaladie.push_back(LireUneMaladie(contenu));
+	}
+	fichier.close();
+}
+
+void Lecture::LireSignatures(vector<Signature> &tabSignature)
+{
+	ifstream fichier(nomFichierPat, ios::in);  // on ouvre en lecture
+	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
+	getline(fichier, contenu);
+	while(getline(fichier, contenu);)  
+	{
+			tabSignature.push_back(LireUneSignature(contenu));
+	}
+	fichier.close();
+}
+
+Signature Lecture::LireUneSignature(string line)
+{
+	Signature laSignature;
+	int pos = 0;
+	int next_pos = line.find(';');
+	Attribut * attr;
+	int i = 0;
+	while(pos != line.length())
+	{
+		if(metadonnees.substr(1,';').equals("ID"))
+		{
+			attr = new AttributID(line.substr(pos,next_pos),metadonnees.substr(0,';').equals("ID"));
+		}
+		else if(metadonnees.substr(1,';').equals("double"))
+		{
+			attr = new AttributNum(line.substr(pos,next_pos),metadonnees.substr(0,';').equals("ID"));
+		}
+		else if(metadonnees.substr(1,';').equals("string"))
+		{
+			attr = new AttributCarac(line.substr(pos,next_pos),metadonnees[i].substr(0,';').equals("ID"));
+		}
+		else
+		{
+			attr = null;
+		}
+		laSignature.AjouterAttribut(attr);
+		pos = next_pos+1;
+		next_pos =  line.find(';');
+		
+		if(next_pos == std::string::npos)
+		{
+		}
+			next_pos = line.length();
+		i++;
+	}
+	
+	return laSignature;
+}
+
+Maladie Lecture::LireUneMaladie(string line)
+{
+	Signature laSignature = LireUneSignature(line.substr(0,line.find_last_of(';')-1));
+	string nom = line.substr(line.find_last_of(';')+1, line.length());
+	Maladie laMaladie = Maladie(nom, laSignature);
+	return laMaladie;
+}
+
 
 Lecture::~Lecture() {
 #ifdef MAP
--- Projet2/Projet2/Lecture.h
+++ Projet2/Projet2/Lecture.h
@@ -5,12 +5,17 @@ class Lecture
 {
 public:
 	Lecture(string fichierMeta, string fichierMal, string fichierPat);
+	Maladie LireUneMaladie(ligne string);
+	Signature LireUnPatient(ligne string);
+	void LirePatients(vector<Patient> &tabPatient);
+	void LireMaladies(vector<Maladie> &tabMaladie);
 	~Lecture();
 
 protected:
 	string nomFichierMeta;
 	string nomFichierMal;
 	string nomFichierPat;
+	vector <string> metadonnees;
 };
 
 #endif
