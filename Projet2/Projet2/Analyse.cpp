997c8490da3d0222008478b60b5f0925b957a8fe
yang hua <huayang970@gmail.com>
2018-05-24 12:10:08 +0200

    analyse yang

--- Projet2/Projet2/Analyse.cpp
+++ Projet2/Projet2/Analyse.cpp
@@ -1,18 +1,31 @@
 using namespace std;
 #include "Analyse.h"
-
+#include "string"
+#include "Maladie.h"
+#include <map>
+#include "Patient.h"
 
 
 Analyse::Analyse()
 {
 }
 
-double risqueNumerique()
+multimap<string, double> FaireAnalyse(Patient unPatient, vector<Maladie>maladies)
 {
+	Signature vSignature = unPatient.getSignature();
+	for (unsigned int i = 0; i < maladies.size(); i++)
+	{
+		risqueNumerique(vSignature)
+	}
 
 }
 
-double risqueCategorique()
+double risqueNumerique(Attribut* attPatient, Attribut* attMaladie)
+{
+	
+}
+
+double risqueCategorique(Attribut* attPatient, Attribut* attMaladie)
 {
 
 }
