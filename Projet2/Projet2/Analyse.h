d8da5a63208dee594acab182238ea95ed12ff4dd
Catalin Gangalic <catalin.gangalic@insa-lyon.fr>
2018-05-25 23:09:39 +0200

    Update Analyse.h
    Added some includes

--- Projet2/Projet2/Analyse.h
+++ Projet2/Projet2/Analyse.h
@@ -1,20 +1,19 @@
 #if ! defined (Analyse_H)
 #define Analyse_H
+#include <vector>
+#include "Maladie.h"
+#include "Patient.h"
+#include "AttributCarac.h"
+#include "AttributNum.h"
 
 class Analyse
 {
 public:
-	Analyse();
-
+    Analyse();
 	Patient FaireAnalyse(Patient unPatient, vector<Maladie> maladies);
-
-	vector<double> risqueNumerique(Attribut* attPatient,vector<Attribut*> attMaladie);
-
+	vector<double> risqueNumerique(Attribut* attPatient, vector<Attribut*> attMaladie);
 	vector<double> risqueCategorique(Attribut* attPatient, vector<Attribut*> attMaladie);
-
-	
 	~Analyse();
-
 
 };
 
