bbb75143102af6ebaa9dfdcfcc0de0b6698db006
user <user@LAPTOP-CA802IFE>
2018-05-26 00:30:11 +0200

    lecture good

--- Projet2/Projet2/Lecture.cpp
+++ Projet2/Projet2/Lecture.cpp
@@ -1,11 +1,13 @@
+
 using namespace std;
+#include <sstream>
 #include <string>
 #include "Lecture.h"
 
 
 Lecture::Lecture(string fichierMeta, string fichierMal, string fichierPat) {
 #ifdef MAP
-     << "Appel au constructeur de <Lecture>" << endl;
+	cout << "Appel au constructeur de <Lecture>" << endl;
 #endif
 	nomFichierMeta = fichierMeta;
 	nomFichierMal = fichierMal;
@@ -19,115 +21,79 @@ Lecture::Lecture(string fichierMeta, string fichierMal, string fichierPat) {
 	{
 			metadonnees.push_back(contenu);
 	}
+
 	fichier.close();
                 
 }
 
 
-vector<Maladie> Lecture::LireMaladies()
+void Lecture::LireMaladies(vector<Maladie> &tabMaladie)
 {
-<<<<<<< HEAD
-    string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
-    ifstream fi;
 
+	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
+    ifstream fi;
     fi.open(nomFichierMal);
     getline(fi, contenu);
-
-    vector<Maladie> tabMaladies;
-    while(contenu != "")
-    {
-
-        getline(fi, contenu);
-        if(contenu != "")
-        {
-            tabMaladies.push_back(LireUneMaladie(contenu));
-         }
-
-    }
-     return tabMaladies;
+    while(fi.good())
+	{
+        cout << contenu<<endl;
+            getline(fi, contenu);
+             cout << contenu<<endl;
+			tabMaladie.push_back(LireUneMaladie(contenu));
+	}
+    fi.close();
 }
 
-vector<Patient> Lecture::LirePatients()
+void Lecture::LireSignatures(vector<Signature> &tabSignature)
 {
     string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
     ifstream fi;
     fi.open(nomFichierPat);
     getline(fi, contenu);
-    vector<Patient> tabPatients;
-    while(contenu != "")
+
+    while(fi.good())
     {
 
         getline(fi, contenu);
-        if(contenu != "")
-        {
-            tabPatients.push_back(Patient(LireUneSignature(contenu)));
-        }
-
-    }
-     return tabPatients;
-=======
-	ifstream fichier(nomFichierMal, ios::in);  // on ouvre en lecture
-	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
-	getline(fichier, contenu);
-    while(getline(fichier, contenu))
-	{
-			tabMaladie.push_back(LireUneMaladie(contenu));
+        tabSignature.push_back(LireUneSignature(contenu));
+        cout << "Apres"<<endl;
 	}
-	fichier.close();
+    fi.close();
 }
 
-void Lecture::LirePatients(vector<Patient> &tabPatient)
-{
-	ifstream fichier(nomFichierPat, ios::in);  // on ouvre en lecture
-	string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
-	getline(fichier, contenu);
-    while(getline(fichier, contenu))
-	{
-            tabPatient.push_back(LireUnPatient(contenu));
-	}
-	fichier.close();
->>>>>>> 12abb18be23baf3314bda12d7462be150940dee0
-}
-
-Patient Lecture::LireUnPatient(string line)
+Signature Lecture::LireUneSignature(string line)
 {
 	Signature laSignature;
-	int pos = 0;
-	int next_pos = line.find(';');
+    stringstream ss;
+    ss.str(line);
+    string info;
 	Attribut * attr;
 	int i = 0;
-	while(pos != line.length())
-	{
-        if(metadonnees[i].substr(1,';') == "ID")
-		{
-            attr = new AttributID(line.substr(pos,next_pos),metadonnees[i].substr(1,';'));
+    while(getline(ss, info,';'))
+    {
+        if(metadonnees[i].substr(metadonnees[i].find(';')+1,metadonnees[i].length()) == "ID")
+        {
+            attr = new AttributID(info,metadonnees[i].substr(0,metadonnees[i].find(';')));
 		}
-        else if((metadonnees[0].substr(1,';') == "double"))
+        else if(metadonnees[i].substr(metadonnees[i].find(';')+1,metadonnees[i].length()) == "double")
 		{
-            attr = new AttributNum(line.substr(pos,next_pos),metadonnees[i].substr(1,';'));
+
+            attr = new AttributNum(info,metadonnees[i].substr(0,metadonnees[i].find(';')));
 		}
-        else if((metadonnees[0].substr(1,';') == "string"))
-		{
-            attr = new AttributCarac(line.substr(pos,next_pos),metadonnees[i].substr(1,';'));
+        else if(metadonnees[i].substr(metadonnees[i].find(';')+1,metadonnees[i].length()) == "string")
+        {
+            attr = new AttributCarac(info,metadonnees[i].substr(0,metadonnees[i].find(';')));
 		}
 		else
-        {
+		{
             attr = NULL;
 		}
-		laSignature.AjouterAttribut(attr);
-		pos = next_pos+1;
-		next_pos =  line.find(';');
-		
-		if(next_pos == std::string::npos)
-		{
-		}
-			next_pos = line.length();
+
+        laSignature.AjouterAttribut(attr);
+
 		i++;
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 12abb18be23baf3314bda12d7462be150940dee0
+    cout << "rendu signature"<<endl;
 	
 	return laSignature;
 
@@ -135,16 +101,9 @@ Patient Lecture::LireUnPatient(string line)
 
 Maladie Lecture::LireUneMaladie(string line)
 {
-<<<<<<< HEAD
-    cout <<line.find_last_of(';')<<endl;
-    cout << line.substr(0,line.find_last_of(';')-1)<<endl;
-    string laS = line.substr(0,line.find_last_of(';')-1);
-    Signature laSignature = LireUneSignature(laS);
-=======
-    Patient laSignature = LireUnPatient(line.substr(0,line.find_last_of(';')-1));
->>>>>>> 12abb18be23baf3314bda12d7462be150940dee0
-	string nom = line.substr(line.find_last_of(';')+1, line.length());
 
+    Signature laSignature = LireUneSignature(line.substr(0,line.find_last_of(';')-1));
+	string nom = line.substr(line.find_last_of(';')+1, line.length());
 	Maladie laMaladie = Maladie(nom, laSignature);
 	return laMaladie;
 }
