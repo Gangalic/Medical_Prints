a7a6e48bc5dd1d2e2954f05cfd5ae50a33ac7229
Catalin Gangalic <catalin.gangalic@insa-lyon.fr>
2018-05-25 23:58:41 +0200

    Update Analyse.cpp
    Small errors of writing

--- Projet2/Projet2/Analyse.cpp
+++ Projet2/Projet2/Analyse.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 #include <map>
 #include <string>
+#include <cmath>
 #include "Analyse.h"
 
 
@@ -80,7 +81,7 @@ vector<double> Analyse::risqueNumerique(Attribut* attPatient, vector<Attribut*> 
 			proba.push_back(1.0);
 		}
 		else {
-			proba.push_back(abs(deltaUnitaire/ delta[i]);
+			proba.push_back(abs(deltaUnitaire/ delta[i]));
 		}
 	}
 
