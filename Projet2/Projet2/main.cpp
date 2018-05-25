04aace7d44a9f7c2c0899191875c8f6c87bd0843
user <user@LAPTOP-CA802IFE>
2018-05-25 23:46:19 +0200

    fgh

--- Projet2/Projet2/main.cpp
+++ Projet2/Projet2/main.cpp
@@ -89,7 +89,12 @@ int main()
 		
 		
 	return 0;
-	*/
+	
+	vector <Signature> tabS;
+	Lecture l("/home/alan/Bureau/fichierTPGL/meta.txt", "/home/alan/Bureau/fichierTPGL/mal.txt", "/home/alan/Bureau/fichierTPGL/p.txt");
+	l.LireSignatures(tabS);*/
+	return 0;
+
 }
 
 
