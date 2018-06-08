faf42b6248474da879c081fd3b3a7655407163e7
Acozou <alan.paugois@gmail.com>
2018-06-08 19:26:18 +0200

    Update Maladie.h

--- Projet2/Projet2/Maladie.h
+++ Projet2/Projet2/Maladie.h
@@ -2,19 +2,57 @@
 #define Maladie_H
 #include "Signature.h"
 
+/**
+ * @brief The Maladie class: Representation d'une maladie
+ */
 class Maladie
 {
 public:
 
+    /**
+     * @brief Maladie: Constructeur avec paramètres
+     * @param leNom: Le nom de la maladie
+     * @param laSignature: La signature de la maladie
+     */
 	Maladie(string leNom, Signature laSignature);
+    
+    /**
+     * @brief AffichageMaladie: Affiche les caractéristiques de la maladie
+     */
 	void AffichageMaladie();
+    
+    /**
+     * @brief getNom: Retourne le nom de la maladie
+     * @return le nom
+     */
 	string getNom();
+    
+    /**
+     * @brief getSignature: Retourne le signature de la maladie
+     * @return la signature
+     */
 	Signature getSignature();
+    
+    /**
+     * @brief destroySignature: Détruit la signature de la maladie
+     */
 	void destroySignature();
+    
+    /**
+     * @brief ~Maladie: Destructeur de la classe
+     */
 	~Maladie();
 
 private:
+    
+    /**
+     * @brief nomMaladie: Le nom de la maladie
+     */
 	string nomMaladie;
+    
+    /**
+     * @brief uneSignature: La signature de la maladie
+     */
 	Signature uneSignature;
 };
 
