ea057a2d7181cec3437a6cf280b29c71690ce086
Acozou <alan.paugois@gmail.com>
2018-06-08 19:23:05 +0200

    Update AttributCarac.h

--- Projet2/Projet2/AttributCarac.h
+++ Projet2/Projet2/AttributCarac.h
@@ -2,17 +2,43 @@
 #define AttributCarac_H
 #include "Attribut.h"
 
+/**
+ * @brief The AttributCarac class: Attribut de type Caractère
+ */
 class AttributCarac : public Attribut {
 
 public:
 
+    /**
+     * @brief AttributCarac: Constructeur par défaut
+     */
 	AttributCarac();
+    
+    /**
+     * @brief AttributCarac: Constructeur avec paramètres
+     * @param valInput : La valeur de l'attribut
+     * @param name: Le nom de l'attribut
+     */
 	AttributCarac(string valInput, string name);
-	virtual void* getValue(); //random
+    
+    /**
+     * @brief getValue
+     * @return un pointeur de la valueur de l'attribut
+     */
+	virtual void* getValue(); 
+    
+    /**
+     * @brief ~AttributCarac: Destructeur de la classe
+     */
 	~AttributCarac();
 
 protected:
+    
+    /**
+     * @brief valeur: La valeur de l'attribut
+     */
 	string valeur;
 };
 
 #endif
+
