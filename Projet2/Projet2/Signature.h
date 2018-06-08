e568d656a2c43c8959452a5d8b0a446464a9e8c6
Acozou <alan.paugois@gmail.com>
2018-06-08 19:28:20 +0200

    Update Signature.h

--- Projet2/Projet2/Signature.h
+++ Projet2/Projet2/Signature.h
@@ -3,17 +3,51 @@
 #include "Attribut.h"
 #include <vector>
 
+/**
+ * @brief The Signature class: Representation d'une signature
+ */
 class Signature
 {
 public:
+    
+    /**
+     * @brief AjouterAttribut: Ajout d'un attribut
+     * @param nouvAttribut: L'attribut à ajouter
+     */
 	void AjouterAttribut(Attribut * nouvAttribut);
+    
+    /**
+     * @brief getIdSignature
+     * @return reourne l'Id de la signature
+     */
 	int getIdSignature();
+    
+    /**
+     * @brief getTabAttributs
+     * @return  Retourne l'ensemble des attributs
+     */
 	vector <Attribut *> getTabAttributs();
+    
+    /**
+     * @brief destroyS: Detruit l'ensemble des attributs
+     */
 	void destroyS();
 
+    /**
+     * @brief Signature: Construteur par défault
+     */
 	Signature();
+    
+    /**
+     * @brief ~Signature: Destructeur de la classe
+     */
 	~Signature();
+    
 protected:
+    
+    /**
+     * @brief tabAttributs: L'ensemble des attributs de la classe
+     */
 	vector <Attribut *> tabAttributs;
 };
 
