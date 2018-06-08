97a68fe0ac4b56526d2b0b9b16e20b6ee47a4b00
Acozou <alan.paugois@gmail.com>
2018-06-08 19:23:36 +0200

    Update AttributID.h

--- Projet2/Projet2/AttributID.h
+++ Projet2/Projet2/AttributID.h
@@ -2,15 +2,36 @@
 #define AttributID_H
 #include "Attribut.h"
 
+/**
+ * @brief The AttributID class: Attribut de type ID
+ */
 class AttributID : public Attribut {
 
 public:
 
+    /**
+     * @brief AttributID: Constructeur avec paramètres
+     * @param valInput: La valeur de l'attribut
+     * @param name: Le nom de l'attribut
+     */
 	AttributID(string valInput, string name);
+    
+    /**
+     * @brief getValue
+     * @return un pointeur de la valueur de l'attribut
+     */
 	virtual void* getValue(); //random
+    
+    /**
+     * @brief ~AttributID: Desructeur de la classe
+     */
 	~AttributID();
 
 protected:
+    
+    /**
+     * @brief valeur: La valeur de l'attribut
+     */
 	int valeur;
 };
 
