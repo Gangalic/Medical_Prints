6a8558cae7f894c552ff29202d8f884b375155fc
Acozou <alan.paugois@gmail.com>
2018-06-08 19:21:49 +0200

    Update Attribut.h

--- Projet2/Projet2/Attribut.h
+++ Projet2/Projet2/Attribut.h
@@ -2,13 +2,35 @@
 #define Attribut_H
 #include <string>
 using namespace std;
+
+/**
+ * @brief The Attribut class: Classe virtuelle d'attribut
+ */
 class Attribut
 {
 public:
+    
+    /**
+     * @brief Attribut: Constructeur par défaut
+     */
 	Attribut();
-	virtual void* getValue(); //random
+    
+    /**
+     * @brief getValue
+     * @return un pointeur de la valueur de l'attribut
+     */
+	virtual void* getValue();
+    
+    /**
+     * @brief ~Attribut: Destructeur de la classe
+     */
 	~Attribut();
+
 protected:
+    
+    /**
+     * @brief nom Le nom de l'attribut
+     */
 	string nom;
 };
 
